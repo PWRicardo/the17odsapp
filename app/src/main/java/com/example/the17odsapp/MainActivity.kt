e6578f61dac38dc7acda7d8e713f1e1e4f129985
Ricardo Hernandez <ricardo031499@gmail.com>
2022-04-16 13:27:46 -0500

    NavHostFragment implementado SIN EXITO

--- app/src/main/java/com/example/the17odsapp/MainActivity.kt
+++ app/src/main/java/com/example/the17odsapp/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.the17odsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.the17odsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,11 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.untext.text = "Hola bro, God Bless you"
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        //setupActionBarWithNavController(navController)
     }
+
 }
