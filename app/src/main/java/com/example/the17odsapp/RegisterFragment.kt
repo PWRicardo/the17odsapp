ab50bf33050439a4d2b1d98e22fbfabaf077b312
Ricardo Hernandez <ricardo031499@gmail.com>
2022-04-16 19:08:13 -0500

    createAccoun() con error en el this

--- app/src/main/java/com/example/the17odsapp/RegisterFragment.kt
+++ app/src/main/java/com/example/the17odsapp/RegisterFragment.kt
@@ -1,14 +1,21 @@
 package com.example.the17odsapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.the17odsapp.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class RegisterFragment : Fragment() {
+    private lateinit var auth : FirebaseAuth
 
     private var _binding: FragmentRegisterBinding? = null
 
@@ -18,9 +25,30 @@ class RegisterFragment : Fragment() {
     override fun onCreateView ( inflater: LayoutInflater, container: ViewGroup?, saveInstanceState: Bundle?): View {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
 
+
+        auth = Firebase.auth
+
         val view = binding.root
 
         return view
+    }
+
+    private fun createAccount( email:String, password:String) {
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this) { task ->
+            if(task.isSuccesful){
+
+                val user = auth.currentUser
+                updateUI(user)
+            } else {
+                Toast.makeText(context!!.applicationContext, "Registro fallido.", Toast.LENGTH_SHORT).show()
+                updateUI(null)
+            }
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
     }
 
 }
