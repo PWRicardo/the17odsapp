271dbcbefbe3541734c40379b07d2ede86e20830
Ricardo Hernandez <ricardo031499@gmail.com>
2022-04-18 17:20:40 -0500

    agregue boton de LOGIN y funciona

--- app/src/main/java/com/example/the17odsapp/RegisterFragment.kt
+++ app/src/main/java/com/example/the17odsapp/RegisterFragment.kt
@@ -15,14 +15,18 @@ import com.google.firebase.ktx.Firebase
 
 
 class RegisterFragment : Fragment() {
-    private lateinit var auth : FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     private var _binding: FragmentRegisterBinding? = null
 
     private val binding get() = _binding!!
 
 
-    override fun onCreateView ( inflater: LayoutInflater, container: ViewGroup?, saveInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        saveInstanceState: Bundle?
+    ): View {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
 
 
@@ -42,22 +46,44 @@ class RegisterFragment : Fragment() {
             val correo = binding.editTextEmail.text.toString()
             val pwd = binding.editTextPwd.text.toString()
 
-            createAccount(correo,pwd)
+            createAccount(correo, pwd)
+        }
+
+        binding.accederButton.setOnClickListener {
+            val correo = binding.editTextEmail.text.toString()
+            val pwd = binding.editTextPwd.text.toString()
+            signIn(correo, pwd)
         }
     }
 
-    private fun createAccount( email:String, password:String) {
+    private fun createAccount(email: String, password: String) {
 
-        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(requireActivity()) { task ->
-            if(task.isSuccessful){
-                val user = auth.currentUser
-                updateUI(user)
-                Toast.makeText(context,"Registro exitoso", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(context, "Registro fallido.", Toast.LENGTH_SHORT).show()
-                updateUI(null)
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                    Toast.makeText(context, "Registro exitoso", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "Registro fallido.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
             }
-        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+
+                    Toast.makeText(context, "entraste bien", Toast.LENGTH_LONG).show()
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(context, "entraste mal", Toast.LENGTH_LONG).show()
+                    updateUI(null)
+                }
+            }
     }
 
     private fun updateUI(user: FirebaseUser?) {
